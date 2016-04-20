fbcb3d1a12ae8dcda341c994bc384a90faf07912
Fabrice Laporte <kraymer@gmail.com>
2016-04-20 11:35:17 +0200

    functions.zsh: refresh prompt every minute (for hour display)

--- _zsh/.zsh/functions.zsh
+++ _zsh/.zsh/functions.zsh
@@ -3,3 +3,11 @@
 #}
 
 #load_file_local ${BASH_SOURCE:-$0}
+
+# Reset prompt every minute to update hour
+TMOUT=60
+TRAPALRM() {
+    if [ "$WIDGET" != "complete-word" ]; then
+        zle reset-prompt
+    fi
+}

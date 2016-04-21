6f7ef46948a6108c64fc413f098f4be3d4b65646
Fabrice Laporte <kraymer@gmail.com>
2016-04-21 11:11:01 +0200

    nano_install.zsh: use https repo address

--- _nano/.zsh/nano_install.zsh
+++ _nano/.zsh/nano_install.zsh
@@ -2,6 +2,7 @@
 install_nanorc() {
     if [ ! -d "~/.nano" ]; then
         echo "Installing '~/.nano' ..."
-        git clone git@github.com:scopatz/nanorc.git ~/.nano
+        git clone https://github.com/scopatz/nanorc.git ~/.nano
     fi
 }
+install_nanorc

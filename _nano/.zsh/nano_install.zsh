753577418139d66858e1236ed5034d55026f6c1b
Fabrice Laporte <kraymer@gmail.com>
2016-04-21 17:10:59 +0200

    nano_install.sh: fix home variable expansion

--- _nano/.zsh/nano_install.zsh
+++ _nano/.zsh/nano_install.zsh
@@ -1,6 +1,6 @@
 # Install nano syntax highlighting files
 install_nanorc() {
-    if [ ! -d "~/.nano" ]; then
+    if [ ! -d "${HOME}/.nano" ]; then
         echo "Installing '~/.nano' ..."
         git clone https://github.com/scopatz/nanorc.git ~/.nano
     fi

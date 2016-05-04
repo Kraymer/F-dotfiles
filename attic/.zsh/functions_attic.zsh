5ccbcbbb14e9c828c90803583900659a9345ebce
Fabrice Laporte <kraymer@gmail.com>
2016-05-04 07:29:49 +0200

    attic: make max_delay attic_last_backup arg optional

--- attic/.zsh/functions_attic.zsh
+++ attic/.zsh/functions_attic.zsh
@@ -1,21 +1,20 @@
 export ATTIC_LAST_BACKUP_FILE=${HOME}/.local/attic_last_backup
 
-# Read date of last execution on filesystem and print it. Print in red if more
-# than MAX_DELAY days elapsed since last backup.
+# Read date of last execution on filesystem and print it.
 #
-# $1 - max delay since last backup in days. Over this period output is printed
-#      in red.
+# $1 - max delay since last backup in days. Over this period, output is printed
+#      in red. Default: 2
 attic_last_backup() {
-    local max_delay=$1; shift
+    local max_delay="${1:-2}"
 
     color=`tput setaf 1`  # red
     if [ ! -f $ATTIC_LAST_BACKUP_FILE ]; then
         attic_last_backup_date='N/A'
     else
-        local attic_last_backup_date=`more $attic_last_backup_file`
+        local attic_last_backup_date=`more $ATTIC_LAST_BACKUP_FILE`
         local days_since_last_backup=`echo $(( ($(date +%s) - $(date --date=${attic_last_backup_date} +%s) )/(60*60*24) ))`
         case $days_since_last_backup in
-        [0-$1])
+        [0-$max_delay])
           color=""
           ;;
         esac
@@ -25,6 +24,6 @@ attic_last_backup() {
 
 
 main() {
-    attic_last_backup 2
+    attic_last_backup
 }
 main "$@"

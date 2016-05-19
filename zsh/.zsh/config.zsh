a4aec53ba53a49df0f3d3e1b44e67070ddfe0f14
Fabrice Laporte <kraymer+github@gmail.com>
2016-05-19 11:59:31 +0200

    Update config.zsh

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -14,9 +14,10 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt SHARE_HISTORY    # share history between sessions ???
 setopt APPEND_HISTORY   # adds history
 
-DISABLE_UNTRACKED_FILES_DIRTY="true" # makes repository status check for large repositories much faster
+# ssh-agent
+eval "$(ssh-agent -s)"
 
-# setopt...
+# set various options
 setopt AUTO_CD
 setopt EXTENDED_GLOB
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
@@ -26,3 +27,5 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt NO_BG_NICE       # don't nice background tasks
 setopt NO_CASE_GLOB     # Case insensitive globbing
 setopt NO_LIST_BEEP
+
+DISABLE_UNTRACKED_FILES_DIRTY="true" # makes repository status check for large repositories much faster

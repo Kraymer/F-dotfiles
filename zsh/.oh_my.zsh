dc6df14f5cbde0f6513f877af57672f73588e731
Fabrice Laporte <fabrice@yescapa.com>
2017-01-13 10:26:02 +0100

    OMZ: add django plugin, change POWERLEVEL9K virtualenv token color

--- zsh/.oh_my.zsh
+++ zsh/.oh_my.zsh
@@ -13,12 +13,13 @@ POWERLEVEL9K_SHORTEN_STRATEGY=truncate_middle
 POWERLEVEL9K_SHORTEN_DELIMITER="*"
 POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_TIME_FORMAT='%D{%H:%M}'
+POWERLEVEL9K_VIRTUALENV_BACKGROUND='150'  # Greenish
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(autojump command-not-found python pip gnu-utils history-substring-search)
+plugins=(autojump command-not-found django gnu-utils history-substring-search pip python)
 
 source $ZSH/oh-my-zsh.sh
 

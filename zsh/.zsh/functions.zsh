cffdef5b3c2344a9bf2f747052c6991a7808bf41
Fabrice Laporte <fabrice@yescapa.com>
2017-03-10 15:49:37 +0100

    Add dirwatch() in zsh/functions.zsh

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -5,3 +5,11 @@ TRAPALRM() {
         zle reset-prompt
     fi
 }
+
+
+dirwatch() {
+    inotifywait -m $1 -e create -e move -e delete |
+        while read thepath action file; do
+            echo "$action $file"
+        done
+}

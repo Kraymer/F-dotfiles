b228bf223308747a3a7307e610d5072741c88815
Fabrice Laporte <kraymer@gmail.com>
2017-04-16 11:01:40 +0200

    Mv python PATH config to python package

--- @mac/.zsh/mac-vars.zshenv
+++ @mac/.zsh/mac-vars.zshenv
@@ -1,4 +1,1 @@
-PY_VERSION=`python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
-export GOPATH=${HOME}/Dev/Go
-export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin/:${GOPATH}/bin:~/Library/Haskell/bin/
-
+export PATH=$PATH:${HOME}/Dev/Go/bin
--- python/.zsh/python.zsh
+++ python/.zsh/python.zsh
@@ -1,5 +1,14 @@
 # virtualenvwrapper
 source virtualenvwrapper.sh
 
-# add `pip install -u` dest path to PATH
+# Add `pip install -u` dest path to PATH
 export PATH=$PATH:~/.local/bin
+
+# Add python2 to PATH
+PY_VERSION=`python2 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
+export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin
+
+# Add python3 to PATH
+PY_VERSION=`python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
+export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin
+

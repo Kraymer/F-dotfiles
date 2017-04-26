c6d62ffc2a933980021888195d8c1d19b9113f26
Fabrice Laporte <fabrice@yescapa.com>
2017-04-26 17:49:46 +0200

    Remove -nH options to grep alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,5 +1,5 @@
 alias atoolx='atool -x'		# extract any archive format
-alias grep='grep -nH --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} --exclude="*.pyc"'
+alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} --exclude="*.pyc"'
 alias h=history
 alias rm='rm -i'
 alias nano='nano -c'

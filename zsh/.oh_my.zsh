c6cd1fda79111692212e9ee202fefcb2c2ba3507
Fabrice Laporte <fabrice@yescapa.com>
2017-07-17 12:16:28 +0200

    zsh: Add zsh-autosuggestions and zsh-syntax-highlighting to plugins

--- zsh/.oh_my.zsh
+++ zsh/.oh_my.zsh
@@ -19,7 +19,7 @@ POWERLEVEL9K_VIRTUALENV_BACKGROUND='150'  # Greenish
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(autojump command-not-found django gnu-utils history-substring-search pip python)
+plugins=(autojump command-not-found django gnu-utils history-substring-search pip python zsh-autosuggestions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 

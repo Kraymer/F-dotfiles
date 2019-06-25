4f3c8935243e7045e72b5cca1814eae7d30c03b1
Fabrice Laporte <kraymergmail.com>
2019-06-25 18:04:00 +0200

    add percol based functions

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -24,3 +24,22 @@ ssh() {
 psql_history() {
     psql -U postgres -c "SELECT (pg_stat_file('base/'||oid ||'/PG_VERSION')).modification, datname FROM pg_database;"
 }
+
+ppgrep() {
+    if [[ $1 == "" ]]; then
+        PERCOL=percol
+    else
+        PERCOL="percol --query $1"
+    fi
+    ps aux | eval $PERCOL | awk '{ print $2 }'
+}
+
+ppkill() {
+    if [[ $1 =~ "^-" ]]; then
+        QUERY=""            # options only
+    else
+        QUERY=$1            # with a query
+        [[ $# > 0 ]] && shift
+    fi
+    ppgrep $QUERY | xargs kill $*
+}

2cc9be9dcf5fff0d1dcc932a8f05360765e5743b
Fabrice Laporte <kraymergmail.com>
2020-03-09 09:54:03 +0100

    python: update PATH

--- python/.zsh/python.zsh
+++ python/.zsh/python.zsh
@@ -4,7 +4,7 @@ export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin
 
 # Add python3 to PATH
 PY_VERSION=`python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
-export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin
+export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin:${HOME}/.local/bin/
 
 # Enable python interpreter history
 export PYTHONSTARTUP=~/.pystartup

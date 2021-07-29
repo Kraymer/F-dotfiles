8ecd7328cd176e4c63505fa70aaf798445a08485
Fabrice Laporte <fabrice@yescapa.com>
2021-07-30 01:00:11 +0200

    python: add pyenv setup

--- python/.zsh/python.zsh
+++ python/.zsh/python.zsh
@@ -1,17 +1,3 @@
-# Add python2 to PATH
-PY_VERSION=`python2 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
-export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin
-
-# Add python3 to PATH
-PY_VERSION=`python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
-export PATH=$PATH:${HOME}/Library/Python/${PY_VERSION}/bin:${HOME}/.local/bin/
-
-# Enable python interpreter history
-export PYTHONSTARTUP=~/.pystartup
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-
-pyenv shell system
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"

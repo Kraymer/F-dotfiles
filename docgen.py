89762b0cdc00ae371df46a6d61c0546a2b186086
Florian Delage <delageflo@ymail.com>
2021-08-16 11:03:24 +0200

    get_package_tree() done

--- docgen.py
+++ docgen.py
@@ -8,7 +8,7 @@ in the <package>/README.md file by the output produced.
 import os
 
 
-def retrieve_dotfiles_packages() -> list:
+def get_dotfiles_packages() -> list:
     """
     Get all the packages containing a README.md file
     from the project root.
@@ -23,3 +23,16 @@ def retrieve_dotfiles_packages() -> list:
             if e.args == (20, "Not a directory"):
                 dotfiles_packages.remove(package)
     return dotfiles_packages
+
+
+def get_package_tree(package) -> str:
+    """
+    Get the package tree in bytes type
+    and return it in utf-8 type.
+    """
+    import subprocess
+    s = subprocess.Popen(f"tree {package} -a",
+                         shell=True,
+                         stdout=subprocess.PIPE)
+    tree = s.stdout.read()
+    return tree.decode("utf-8")

4d19314636d7baece763f568031bbb0c908c2839
Florian Delage <delageflo@ymail.com>
2021-08-16 11:06:18 +0200

    update_readme() done

--- docgen.py
+++ docgen.py
@@ -36,3 +36,24 @@ def get_package_tree(package) -> str:
                          stdout=subprocess.PIPE)
     tree = s.stdout.read()
     return tree.decode("utf-8")
+
+
+BLOCK_PACKAGE = "Package tree:\n"
+ENDBLOCK_PACKAGE = "---\n"
+
+def update_readme(package, tree):
+    """
+    Open the README.md contained in a package,
+    delete the existing tree and replace it with the updated tree.
+    """
+    with open(f"./{package}/README.md") as file:
+        lines_list = file.readlines()
+        block_index = lines_list.index(BLOCK_PACKAGE) + 1
+        endblock_index = lines_list.index(ENDBLOCK_PACKAGE) - 1
+        for i in range(block_index, endblock_index):
+            lines_list[i] = ""
+        lines_list[block_index] = "\n"
+        lines_list[block_index+1] = tree
+    with open(f"./{package}/README.md", "w") as file:
+        new_file_contents = "".join(lines_list)
+        file.write(new_file_contents)

1f4f9b0073e146355806751d4de02070be4a8a62
Fabrice <kraymer@gmail.com>
2021-10-22 18:35:06 +0200

    zsh: add 'lc' alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} --exclude="
 alias h='fc -li 1000'
 alias rm='rm -i'
 alias nano='nano -c'
+alias lc='$($(fc -ln -1) |& tail -1)'
 
 # suffix aliases http://zshwiki.org/home/examples/aliassuffix
 alias -s log=nano
